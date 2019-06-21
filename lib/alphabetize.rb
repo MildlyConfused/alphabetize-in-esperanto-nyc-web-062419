ee24e56f54c41666c95c98e96c34c8e02391929c
Ben Braunstein <benzbraunstein@gmail.com>
2019-06-21 14:38:41 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,9 @@
 def alphabetize(arr)
   # code here
+  alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".split("")
+  alphabetize = arr.sort_by do |word|
+    word.split('').map do |letter|
+      alphabet.index(letter)
+    end
+  end
 end
